ca6a9eff984e14e12b46513ea9f8e1d4c3c44757
samoen@gmail.com <samoen@gmail.com>
2018-11-17 14:26:32 +0800

    boiling down the go

--- chatserver.go
+++ chatserver.go
@@ -2,34 +2,74 @@ package main
 
 import (
 	"bytes"
-	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 func main() {
-	hub := newHub()
-	go hub.run()
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/home.html")
-
-	})
-	http.HandleFunc("/chatfront", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/chatfront.html")
-
-	})
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
-		client.hub.register <- client
-		go client.writePump()
-		go client.readPump()
-	})
+	clients := make(map[*client]bool)
+	http.HandleFunc(
+		"/home",
+		func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, "static/home.html")
+		},
+	)
+	http.HandleFunc(
+		"/ws",
+		func(w http.ResponseWriter, r *http.Request) {
+			conn, err := upgrader.Upgrade(w, r, nil)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			cliento := &client{conn: conn, send: make(chan []byte, 256)}
+			clients[cliento] = true
+			go func() {
+				ticker := time.NewTicker(pingPeriod)
+				defer func() {
+					ticker.Stop()
+					cliento.conn.Close()
+				}()
+				for {
+					<-ticker.C
+					cliento.conn.SetWriteDeadline(time.Now().Add(writeWait))
+					err := cliento.conn.WriteMessage(websocket.PingMessage, nil)
+					if err != nil {
+						return
+					}
+				}
+			}()
+			go func() {
+				defer func() {
+					delete(clients, cliento)
+					close(cliento.send)
+					cliento.conn.Close()
+				}()
+				cliento.conn.SetReadLimit(maxMessageSize)
+				cliento.conn.SetReadDeadline(time.Now().Add(pongWait))
+				cliento.conn.SetPongHandler(func(string) error { cliento.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+				for {
+					_, message, err := cliento.conn.ReadMessage()
+					if err != nil {
+						if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+							log.Printf("error: %v", err)
+						}
+						break
+					}
+					message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+					for clienty := range clients {
+						// clienty.conn.SetWriteDeadline(time.Now().Add(writeWait))
+						writer, _ := clienty.conn.NextWriter(websocket.TextMessage)
+						writer.Write(message)
+						writer.Close()
+					}
+				}
+			}()
+		},
+	)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
@@ -40,10 +80,11 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+
 const (
-	writeWait = 10 * time.Second
-	pongWait = 60 * time.Second
-	pingPeriod = (pongWait * 9) / 10
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
 	maxMessageSize = 512
 )
 
@@ -51,103 +92,8 @@ var (
 	newline = []byte{'\n'}
 	space   = []byte{' '}
 )
-type Client struct {
-	hub *Hub
+
+type client struct {
 	conn *websocket.Conn
 	send chan []byte
-}
-func (c *Client) readPump() {
-	defer func() {
-		c.hub.unregister <- c
-		c.conn.Close()
-	}()
-	c.conn.SetReadLimit(maxMessageSize)
-	c.conn.SetReadDeadline(time.Now().Add(pongWait))
-	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, message, err := c.conn.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
-			break
-		}
-		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
-		c.hub.broadcast <- message
-	}
-}
-
-func (c *Client) writePump() {
-	ticker := time.NewTicker(pingPeriod)
-	defer func() {
-		ticker.Stop()
-		c.conn.Close()
-	}()
-	for {
-		select {
-		case message, ok := <-c.send:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if !ok {
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
-			w.Write(message)
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.send)
-			}
-			if err := w.Close(); err != nil {
-				return
-			}
-		case <-ticker.C:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				return
-			}
-		}
-	}
-}
-
-type Hub struct {
-	clients map[*Client]bool
-	broadcast chan []byte
-	register chan *Client
-	unregister chan *Client
-}
-
-func newHub() *Hub {
-	return &Hub{
-		broadcast:  make(chan []byte),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
-		clients:    make(map[*Client]bool),
-	}
-}
-
-func (h *Hub) run() {
-	for {
-		select {
-		case client := <-h.register:
-			h.clients[client] = true
-		case client := <-h.unregister:
-			if _, ok := h.clients[client]; ok {
-				delete(h.clients, client)
-				close(client.send)
-			}
-		case message := <-h.broadcast:
-			for client := range h.clients {
-				select {
-				case client.send <- message:
-				default:
-					close(client.send)
-					delete(h.clients, client)
-				}
-			}
-		}
-	}
 }
